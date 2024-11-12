7e8e4e3541cb7debc08c1fda9e3e1945c8262bf5
Raul Werner <qrananico25@gmail.com>
2024-11-12 20:48:52 -0300

    0.7.1
    Inicio da tela de Ranking

--- screens/PageRank.js
+++ screens/PageRank.js
@@ -6,6 +6,10 @@ const PageRank = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <BackButton navigation={navigation} />
+      <View style={styles.titleContainer}>
+            <Text style={[styles.titleRed]}>Ranking</Text>
+            <Text style={styles.titleBlack}>regional</Text>
+      </View>
     </View>
   );
 };
@@ -13,6 +17,28 @@ const PageRank = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  titleContainer: {
+    alignItems: 'center',
+    marginHorizontal: 20, 
+    marginTop: 104,
+    marginRight: 122,
+    marginLeft:31,
+  },
+  titleRed: {
+    fontSize: 64,
+    fontWeight: 'bold',
+    fontFamily: 'Montserrat-SemiBold',
+    textAlign: 'center',
+    color: '#FD0100',
+  },
+  titleBlack: {
+    fontSize: 64,
+    marginTop: -20, //gambiarra
+    fontWeight: 'bold',
+    fontFamily: 'Montserrat-SemiBold',
+    textAlign: 'center',
+    color: '#1C191F',
   },
 });
 
