ce8e6c876e837e6c1b28b4407e57c824414a297b
Raul Werner <qrananico25@gmail.com>
2024-11-18 21:02:56 -0300

    version 0.8
    Ajustes da tela de Explorar eventos, falta ajustar para incluir os eventos do banco de dados

--- screens/PageRank.js
+++ screens/PageRank.js
@@ -1,14 +1,38 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 import React from 'react';
 import BackButton from '../components/BackButton.js';
+import { BlurView } from 'expo-blur';
 
 const PageRank = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <BackButton navigation={navigation} />
       <View style={styles.titleContainer}>
-            <Text style={[styles.titleRed]}>Eventos</Text>
-            <Text style={styles.titleBlack}>ativos</Text>
+            <Text style={[styles.titleRed]}>Explore</Text>
+            <Text style={styles.titleBlack}>os</Text>
+            <Text style={styles.titleBlack}>eventos</Text>
+      </View>
+      <View style={styles.viewEvents}>
+        <Image 
+            source={require("../assets/images/eventoCarros1.jpg")}
+            style={styles.eventImage}
+        />
+        <BlurView intensity={80} tint="dark" style={styles.absoluteBlur}>
+          <Text style={styles.bodyText}>Nome do Evento</Text>
+          <Text style={styles.bodyText}>Horário</Text>
+          <Text style={styles.bodyText}>Local</Text>
+        </BlurView>
+      </View>
+      <View style={styles.viewEvents}>
+        <Image 
+            source={require("../assets/images/eventoCarros2.jpg")}
+            style={styles.eventImage}
+        />
+        <BlurView intensity={80} tint="dark" style={styles.absoluteBlur}>
+          <Text style={styles.bodyText}>Nome do Evento</Text>
+          <Text style={styles.bodyText}>Horário</Text>
+          <Text style={styles.bodyText}>Local</Text>
+        </BlurView>
       </View>
     </View>
   );
@@ -17,6 +41,7 @@ const PageRank = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
   },
   titleContainer: {
     alignItems: 'center',
@@ -31,11 +56,39 @@ const styles = StyleSheet.create({
   },
   titleBlack: {
     fontSize: 64,
-    marginTop: -20, //gambiarra
+    marginTop: -20,
     fontWeight: 'bold',
     fontFamily: 'Montserrat-SemiBold',
     textAlign: 'center',
     color: '#1C191F',
+  },
+  bodyText: {
+    textAlign: 'center',
+    fontSize: 18,
+    fontFamily: 'Montserrat-SemiBold',
+    color: '#FAFEFF',
+  },
+  absoluteBlur: {
+    position: 'relative',
+    minWidth: '50%',
+    minHeight: '10%',
+    borderRadius: 25,
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  viewEvents: {
+    position: 'relative',
+    width: '90%',
+    minHeight: '10%',
+    borderRadius: 25,
+    overflow: 'hidden',
+    marginTop: '10%',
+  },
+  eventImage: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute'
   },
 });
 
